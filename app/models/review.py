ca81a138b2002f1683a82157b3c4f9f319ed81b6
James Kang <167275921+seongHyun-james-kang@users.noreply.github.com>
2025-07-11 21:59:29 -0400

    working on review.py

---
+++ app/models/review.py
@@ -0,0 +1,16 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    business_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('businesses.id')), nullable=False)
+    rating = db.Column(db.Integer, nullable=False) # 1 to 5
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
