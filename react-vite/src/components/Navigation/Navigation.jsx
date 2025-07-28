98d89c101352cf53da09102ae3ebf1e957f91f80
Kurisone <wrathscythe@icloud.com>
2025-07-27 19:39:30 -0700

    Added Navigation

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,18 +1,32 @@
 import { NavLink } from "react-router-dom";
+import OpenModalButton from "./OpenModalButton";
+import LoginFormModal from "./LoginFormModal/LoginFormModal";
+import SignupFormModal from "./SignupFormModal/SignupFormModal";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 function Navigation() {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-
-      <li>
+    <nav className="nav-container">
+      <div className="nav-left">
+        <NavLink to="/" className="nav-logo">
+          <h1>JET-Biz-Locator</h1>
+        </NavLink>
+      </div>
+      <div className="nav-right">
+        <OpenModalButton
+          buttonText="Log In"
+          modalComponent={<LoginFormModal />}
+          buttonClass="nav-button login"
+        />
+        <OpenModalButton
+          buttonText="Sign Up"
+          modalComponent={<SignupFormModal />}
+          buttonClass="nav-button signup"
+        />
         <ProfileButton />
-      </li>
-    </ul>
+      </div>
+    </nav>
   );
 }
 
