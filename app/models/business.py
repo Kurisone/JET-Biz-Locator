ef3af21c6c160a25e4a91c89b597cab14f5bc5c2
Kurisone <wrathscythe@icloud.com>
2025-08-04 00:48:48 -0700

    Fixing models for render to work

--- app/models/business.py
+++ app/models/business.py
@@ -9,7 +9,7 @@ class Business(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     address = db.Column(db.String(255), nullable=False)
